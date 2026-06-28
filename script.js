bd25afa19a6546d4cb963c54eff4914248d6125a
Felipe <huallysonfelipe@gmail.com>
2026-06-28 16:48:38 -0300

    Colocando Funções de Adicionar e Finalizar

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function trocarImagem(img) {
-    document.querySelector(".produto-imagem img").src = "./img_polo/" + img;
+
+    const imagem = document.querySelector(".produto-imagem img");
+
+    imagem.style.opacity = "0";
+
+    setTimeout(() => {
+        imagem.src = "./img_polo/" + img;
+        imagem.style.opacity = "1";
+    }, 200);
+}
+
+function selecionarCor(btn) {
+    document.querySelectorAll(".cor")
+    .forEach(c => c.classList.remove("ativa"));
+
+    btn.classList.add("ativa");
 }
 
 //tamanho padrão
 let tamanhoSelecionado = "M";
 
 function selecionarTamanho(btn) {
-    document.querySelectorALL(".tamanho button")
+    document.querySelectorAll(".tamanho button")
     .forEach(b => b.classList.remove("ativo"));
 
     btn.classList.add("ativo");
@@ -14,19 +29,50 @@ function selecionarTamanho(btn) {
     tamanhoSelecionado = btn.innerText;
 }
 
-function EnviarWhatsApp() {
+let carrinho = [];
+
+function adicionarCarrinho() {
+
     let produto = "Polo Premium Preta";
+
     let preco = "129,90";
-    let quantidade = document.querySelector("input[type='number']").value;
 
-    let msg = `Olá! Quero comprar:
+    let quantidade =
+    document.querySelector("input[type='number']").value;
 
-    Produto: ${produto}
-    Tamanho: ${tamanhoSelecionado}
-    Quantidade: ${quantidade}
-    Preço: R$ ${preco}`;
+    let item = {
+        produto,
+        preco,
+        tamanho: tamanhoSelecionado,
+        quantidade
+    };
 
-    let url = `https://wa.me/5581999999999?text=${encodeURIComponent(msg)}`;
+    carrinho.push(item);
+
+    document.querySelector(".mensagem-carrinho")
+    .innerText = "Produto adicionado ao carrinho!";
+}
+
+
+function EnviarWhatsApp() {
+
+    if(carrinho.length === 0) {
+        alert("Seu carrinho está vazio!");
+        return;
+    }
+
+    let msg = "Olá! Quero comprar:%0A%0A";
+
+    carrinho.forEach(item => {
+
+        msg += `Produto: ${item.produto}%0A`;
+        msg += `Tamanho: ${item.tamanho}%0A`;
+        msg += `Quantidade: ${item.quantidade}%0A`;
+        msg += `Preço: R$ ${item.preco}%0A%0A`;
+
+    });
+
+    let url = `https://wa.me/558185377245?text=${msg}`;
 
     window.open(url, "_blank");
 }
